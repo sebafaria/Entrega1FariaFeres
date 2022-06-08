cceb7b1cc165b8d6cfa74f1d97e6b9ad6cef6bef
Sebastian Faria <sebastianlfaria@gmail.com>
2022-06-08 01:27:26 -0300

    nuevas clases

--- obras/admin.py
+++ obras/admin.py
@@ -1,9 +1,11 @@
+from multiprocessing.connection import Client
 from django.contrib import admin
 from obras.models import Obra
 from obras.models import Clientes
 from obras.models import Empleados
 
-
 # Register your models here.
 
 admin.site.register(Obra)
+admin.site.register(Clientes)
+admin.site.register(Empleados)
