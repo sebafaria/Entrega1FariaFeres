b06fbb3b35afff8ff5f8eceedbc8814e081b25b7
Sebastian Faria <sebastianlfaria@gmail.com>
2022-06-08 02:50:33 -0300

    asd

--- obras/urls.py
+++ obras/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from obras.views import crear_obra
+from obras.views import crear_obra, search_obra
 
 
 
 
 urlpatterns = [
     path('crear-obra/', crear_obra, name='crear-obra'),
-    
+    path('search_products/', search_obra, name='search_obra')
     ]
--- obras/views.py
+++ obras/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from obras.models import Obra
 
 
 
 
 def crear_obra(request):
     return render(request, 'crear_obra.html')
+
+def search_obra(request):
+    obra = Obra.objects.filter(name__icontains=request.GET['search'])
+    if obra.exists():
+        context={'obra':obra}
+    else:
+        context={'errors':'No se encontro la obra'}
+    return render(request,'search_obra.html',context=context)
