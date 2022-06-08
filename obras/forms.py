5fc85decb5b5e13b22f83913acb4cf298b1b62bd
Sebastian Faria <sebastianlfaria@gmail.com>
2022-06-08 03:09:57 -0300

    asd

--- obras/forms.py
+++ obras/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from obras.models import Obra
 
-class Obra_form(forms.Modelform):
+class Obra_form(forms.Form):
     class Meta:
         model = Obra
         fields = '__all__'
--- obras/urls.py
+++ obras/urls.py
@@ -6,5 +6,5 @@ from obras.views import crear_obra, search_obra
 
 urlpatterns = [
     path('crear-obra/', crear_obra, name='crear-obra'),
-    path('search_products/', search_obra, name='search_obra')
+    path('search_obra/', search_obra, name='search_obra')
     ]
