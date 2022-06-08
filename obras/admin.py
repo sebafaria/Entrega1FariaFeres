975e9bd2c7a7f2e80586a9772cfcdb24d9061d30
Sebastian Faria <sebastianlfaria@gmail.com>
2022-06-08 01:24:42 -0300

    nuevas clases

--- obras/admin.py
+++ obras/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from obras.models import Obra
+from obras.models import Clientes
+from obras.models import Empleados
+
 
 # Register your models here.
 
--- obras/models.py
+++ obras/models.py
@@ -8,3 +8,16 @@ class Obra(models.Model):
     nombre = models.CharField(max_length=100)
     fecha = models.DateField()
 
+class Clientes(models.Model):
+    nombrecliente = models.CharField(max_length=100)
+    direccionfiscal = models.CharField(max_length=100)
+    cuit = models.IntegerField()
+    fechaingreso = models.DateField()
+    antiguedad = models.IntegerField()
+
+class Empleados(models.Model):
+    nombreapellido = models.CharField(max_length=100)
+    domicilio = models.CharField(max_length=100)
+    dni = models.IntegerField()
+    fechaing = models.DateField()
+    antig = models.IntegerField()
